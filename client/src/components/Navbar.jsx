f0e2fac96f25124c520a0456164c08f84f89d355
Kartik Kankurte <kartikkankurte@gmail.com>
2024-01-20 03:40:05 +0530

    made navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Button from '@mui/material/Button';
+import './Navbar.css'
+import {Link} from 'react-router-dom'
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function ElevateAppBar(props) {
+  return (
+    <React.Fragment>
+      <ElevationScroll {...props}>
+        <AppBar className='nav'>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to='/' className='link'><img src='icon.png' width={'30px'} alt='logo'/><b>Bloggify</b></Link>
+            </Typography>
+            <Link to='/login' className='link'><Button color="inherit">Login</Button></Link>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+    </React.Fragment>
+  );
+}
