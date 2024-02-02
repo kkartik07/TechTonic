4dc01f2b1a0900fcafc2eb1e48e57888301ead18
Kartik Kankurte <kartikkankurte@gmail.com>
2024-02-02 20:09:35 +0530

    added carousal for trending posts

---
+++ client/src/components/Carousal.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PostCard from './PostCard';
+import './Carousal.css'
+
+const Carousal = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/posts`);
+        if (!response) return;
+
+        const newPosts = response.data.sort((a, b) => b.popularity - a.popularity);
+        const tPosts = newPosts.slice(0, 10);
+        setPosts(tPosts);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    getPosts();
+  }, []);
+
+  return (
+    <div className="c">
+      <div className='card-home'>
+      {posts.map((post) => (
+      <PostCard post={post} key={post._id} className='pc'/>
+      ))}
+      </div>
+    </div>
+  );
+};
+
+export default Carousal;
